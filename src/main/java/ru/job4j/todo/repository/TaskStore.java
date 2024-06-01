61eb01d08a42d629d6ed93288b01a7f6f38a1028
Pavel <heppie@yandex.ru>
2024-06-01 15:49:17 +0300

    6. Лямбды и шаблон Command [#49295]

--- src/main/java/ru/job4j/todo/repository/TaskStore.java
+++ src/main/java/ru/job4j/todo/repository/TaskStore.java
@@ -1,196 +1,101 @@
 package ru.job4j.todo.repository;
 
 import lombok.AllArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.Task;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Repository
 @AllArgsConstructor
 public class TaskStore implements Store {
-
-    private static final Logger LOG = LoggerFactory.getLogger(TaskStore.class.getName());
-    private final SessionFactory sf;
+    private final CrudRepository crudRepository;
 
     @Override
     public Optional<Task> add(Task task) {
-        Session session = sf.openSession();
-        Optional<Task> rsl = Optional.empty();
-        try {
-            session.beginTransaction();
-            session.save(task);
-            session.getTransaction().commit();
-            rsl = Optional.of(task);
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            LOG.warn("Объект task не был сохранен в БД по причине возникновения исключения: " + e.getMessage(), e);
-        } finally {
-            session.close();
-        }
-        return rsl;
+        crudRepository.run(session -> session.save(task));
+        return Optional.of(task);
     }
 
     @Override
     public boolean update(Integer taskId) {
-        Session session = sf.openSession();
-        boolean rsl = false;
-        try {
-            session.beginTransaction();
-            int affectedRows = session.createQuery("""
+        return crudRepository.runAndReturnBool(
+                """
                     UPDATE Task
                     SET
                     done = :fDone
                     WHERE id = :fId
-                    """)
-                    .setParameter("fDone", true)
-                    .setParameter("fId", taskId)
-                    .executeUpdate();
-            session.getTransaction().commit();
-            rsl = affectedRows > 0;
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            LOG.warn("Объект task не был обновлен в БД по причине возникновения исключения: " + e.getMessage(), e);
-        } finally {
-            session.close();
-        }
-        return rsl;
+                    """,
+                Map.of(
+                        "fDone", true,
+                        "fId", taskId
+                )
+        );
     }
 
     @Override
     public boolean update(Task task) {
-        Session session = sf.openSession();
-        boolean rsl = false;
-        try {
-            session.beginTransaction();
-            int affectedRows = session.createQuery("""
+        return crudRepository.runAndReturnBool(
+                """
                     UPDATE Task
                     SET
                     description = :fDescription
                     WHERE id = :fId
-                    """)
-                    .setParameter("fDescription", task.getDescription())
-                    .setParameter("fId", task.getId())
-                    .executeUpdate();
-            session.getTransaction().commit();
-            rsl = affectedRows > 0;
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            LOG.warn("Объект task не был обновлен в БД по причине возникновения исключения: " + e.getMessage(), e);
-        } finally {
-            session.close();
-        }
-        return rsl;
+                    """,
+                Map.of(
+                        "fDescription", task.getDescription(),
+                        "fId", task.getId()
+                )
+        );
     }
 
     @Override
     public boolean delete(Integer taskId) {
-        Session session = sf.openSession();
-        boolean rsl = false;
-        try {
-            session.beginTransaction();
-            var affectedRows = session.createQuery(
-                            "DELETE Task WHERE id = :fId")
-                    .setParameter("fId", taskId)
-                    .executeUpdate();
-            session.getTransaction().commit();
-            rsl = affectedRows > 0;
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            LOG.warn("Объект task не был удален из БД по причине возникновения исключения: " + e.getMessage(), e);
-        } finally {
-            session.close();
-        }
-        return rsl;
+        return crudRepository.runAndReturnBool(
+                "DELETE Task WHERE id = :fId",
+                Map.of(
+                        "fId", taskId
+                )
+        );
     }
 
     @Override
     public Optional<Task> findById(Integer taskId) {
-        Session session = sf.openSession();
-        Optional<Task> rsl = Optional.empty();
-        try {
-            session.beginTransaction();
-            Query<Task> query = session.createQuery(
-                    "from Task t where t.id = :fId", Task.class);
-            query.setParameter("fId", taskId);
-            session.getTransaction().commit();
-            rsl = query.uniqueResultOptional();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            LOG.warn("Возникло исключение при поиске записи в БД по id: " + e.getMessage(), e);
-        } finally {
-            session.close();
-        }
-        return rsl;
+        return crudRepository.optional(
+                "FROM Task t WHERE t.id = :fId", Task.class,
+                Map.of(
+                        "fId", taskId
+                )
+        );
     }
 
     @Override
     public Collection<Task> findAll() {
-        Session session = sf.openSession();
-        List<Task> rsl = Collections.emptyList();
-        try {
-            session.beginTransaction();
-            var query = session.createQuery("from Task", Task.class);
-            rsl = query.list();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            LOG.warn("Возникло исключение при поиске всех записей в БД: " + e.getMessage(), e);
-        } finally {
-            session.close();
-        }
-        return rsl;
+        return crudRepository.query(
+                "FROM Task", Task.class
+        );
     }
 
     @Override
     public Collection<Task> findByCurrentDate() {
-        Session session = sf.openSession();
-        List<Task> rsl = Collections.emptyList();
-        try {
-            session.beginTransaction();
-            var query = session.createQuery(
-            """
+        return crudRepository.query(
+                """
             FROM Task
             WHERE created BETWEEN :fStartDateTime AND :fEndDateTime
             """,
-                    Task.class)
-                    .setParameter("fStartDateTime", LocalDateTime.now().minusDays(5))
-                    .setParameter("fEndDateTime", LocalDateTime.now());
-            rsl = query.list();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            LOG.warn("Возникло исключение при поиске всех записей в БД по фильтру новые: " + e.getMessage(), e);
-        } finally {
-            session.close();
-        }
-        return rsl;
+                Task.class,
+                Map.of(
+                        "fStartDateTime", LocalDateTime.now().minusDays(5),
+                        "fEndDateTime", LocalDateTime.now()
+                )
+        );
     }
 
     @Override
     public Collection<Task> findByDone() {
-        Session session = sf.openSession();
-        List<Task> rsl = Collections.emptyList();
-        try {
-            session.beginTransaction();
-            var query = session.createQuery("FROM Task WHERE done = TRUE", Task.class);
-            rsl = query.list();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            LOG.warn("Возникло исключение при поиске записей в БД по фильтру done: " + e.getMessage(), e);
-        } finally {
-            session.close();
-        }
-        return rsl;
+        return crudRepository.query(
+                "FROM Task WHERE done = TRUE", Task.class
+        );
     }
 }
